ac5eb257b09f4138f4f20a3e8eb032830ae6d839
Tim Savannah <kata198@gmail.com>
2019-05-13 11:33:59 -0400

    Bump to 4.3.2 to re-release with up-to-date pydoc link.

--- func_timeout/__init__.py
+++ func_timeout/__init__.py
@@ -6,8 +6,8 @@
 '''
 
 
-__version__ = '4.3.1'
-__version_tuple__ = (4, 3, 1)
+__version__ = '4.3.2'
+__version_tuple__ = (4, 3, 2)
 
 __all__ = ('func_timeout', 'func_set_timeout', 'FunctionTimedOut', 'StoppableThread')
 
