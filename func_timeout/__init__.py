1eba58f599f746880a48e858f2dd005efa1a3cf5
Tim Savannah <kata198@gmail.com>
2019-08-19 16:36:57 -0400

    4.3.4

--- func_timeout/__init__.py
+++ func_timeout/__init__.py
@@ -6,8 +6,8 @@
 '''
 
 
-__version__ = '4.3.3'
-__version_tuple__ = (4, 3, 3)
+__version__ = '4.3.4'
+__version_tuple__ = (4, 3, 4)
 
 __all__ = ('func_timeout', 'func_set_timeout', 'FunctionTimedOut', 'StoppableThread')
 
